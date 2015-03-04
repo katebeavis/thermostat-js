2f049a0c0b6345b2ba1db61e05e45fa635010d1b
Kate <katebeavis84@gmail.com>
2015-03-04 16:51:47 +0000

    Can change temp on front end

--- src/interface.js
+++ src/interface.js
@@ -4,4 +4,14 @@ $(document).ready(function() {
 
   $('#temperature').text(thermo.temperature);
 
+  $('.increaseTemperature').click( function() {
+  	thermo.increase();
+  	$('#temperature').text(thermo.temperature);
+  });
+
+  $('.decreaseTemperature').click( function() {
+    thermo.decrease();
+    $('#temperature').text(thermo.temperature);
+  });
+
 });
